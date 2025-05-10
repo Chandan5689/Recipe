8b7cbdb492f1abdcaeb4eb693e65fd16591be427
Chandan981 <chandantiwari8889@gmail.com>
2025-05-10 13:09:26 +0545

    added ScrollToTop component for smooth scrolling

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "./components/Header";
-
 import Footer from "./components/Footer";
 import NewsLetter from "./pages/NewsLetter/NewsLetter";
 import { Analytics } from "@vercel/analytics/react"
+import ScrollToTop from "./components/ScrollControlling/ScrollToTop";
 
 function Layout() {
   
   return (
     
       <div className="bg-gray-50 min-h-screen">
+        <ScrollToTop />
         <Header />
-        
         <Outlet />
-        
         <NewsLetter />
         <Footer />
         <Analytics />
