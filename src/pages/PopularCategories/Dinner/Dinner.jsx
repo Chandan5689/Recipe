7cbca7e7ddacc7b823351e847b4ef327665aee79
Chandan981 <chandantiwari8889@gmail.com>
2025-05-10 13:19:00 +0545

    added props data for title

--- src/pages/PopularCategories/Dinner/Dinner.jsx
+++ src/pages/PopularCategories/Dinner/Dinner.jsx
@@ -15,6 +15,7 @@ function Dinner() {
         bgImage={
           "https://readdy.ai/api/search-image?query=Elegant%20dinner%20spread%20with%20roasted%20meats%2C%20vegetable%20sides%2C%20pasta%20dishes%2C%20and%20fresh%20bread.%20Warm%20ambient%20lighting%20creating%20a%20cozy%20dining%20atmosphere.%20Food%20beautifully%20plated%20on%20ceramic%20dishes%20with%20subtle%20garnishes.%20Rich%2C%20inviting%20colors%20and%20textures%20showcasing%20sophisticated%20home%20cooking.&width=400&height=300&seq=9&orientation=landscape"
         }
+        title={`Enjoy your Dinner everyday with different dishes`}
       />
      <EachCategory filteredCategory={filteredCategory} exploreCategoryTitle={'Dinner'} />
     </>
