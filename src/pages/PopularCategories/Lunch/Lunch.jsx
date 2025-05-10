cb0ebe744773a47ce9ccd9060242f841af2db7e2
Chandan981 <chandantiwari8889@gmail.com>
2025-05-10 13:18:05 +0545

    added props data for title

--- src/pages/PopularCategories/Lunch/Lunch.jsx
+++ src/pages/PopularCategories/Lunch/Lunch.jsx
@@ -3,7 +3,6 @@ import Hero from "../../../components/Hero";
 import RecipesData from "../../../assets/RecipesData";
 import EachCategory from "../EachCategory";
 function Lunch() {
-  
   const exploreRecipescategory = RecipesData;
   const filteredCategory = exploreRecipescategory.filter(
     (e) => e.category === "Lunch"
@@ -14,8 +13,12 @@ function Lunch() {
         bgImage={
           "https://readdy.ai/api/search-image?query=Lunch%20spread%20featuring%20colorful%20salads%2C%20sandwiches%2C%20grain%20bowls%2C%20and%20fresh%20vegetables.%20Bright%20natural%20lighting%20with%20a%20clean%2C%20minimalist%20aesthetic.%20Food%20arranged%20on%20a%20light%20wooden%20table%20with%20cloth%20napkins%20and%20simple%20tableware.%20Perfect%20balance%20of%20healthy%20and%20satisfying%20meal%20options.&width=400&height=300&seq=8&orientation=landscape"
         }
+        title={`Enjoy your Lunch everyday with different dishes`}
       />
-      <EachCategory filteredCategory={filteredCategory} exploreCategoryTitle={'Lunch'} />
+      <EachCategory
+        filteredCategory={filteredCategory}
+        exploreCategoryTitle={"Lunch"}
+      />
     </>
   );
 }
