4cc2945e924595999ff4810e3c5c9a274a151e72
Chandan981 <chandantiwari8889@gmail.com>
2025-05-10 13:21:44 +0545

    added index element in route and changed package to react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, Route, RouterProvider } from "react-router";
-import { createRoutesFromElements } from "react-router";
+import { createBrowserRouter, Route, RouterProvider,createRoutesFromElements } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import BreakFast from "./pages/PopularCategories/Breakfast/Breakfast.jsx";
@@ -14,7 +13,7 @@ import RecipeDetails from "./pages/RecipeDetails/RecipeDetails.jsx";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
+      <Route index element={<Home />} />
       <Route path="/breakfast" element={<BreakFast />} />
       <Route path="/dinner" element={<Dinner />} />
       <Route path="/lunch" element={<Lunch />} />
