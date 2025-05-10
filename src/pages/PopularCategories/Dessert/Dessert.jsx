4f0484087f6790fb15a8f02efabce3cf17ef9dfb
Chandan981 <chandantiwari8889@gmail.com>
2025-05-10 13:18:40 +0545

    added props data for title

--- src/pages/PopularCategories/Dessert/Dessert.jsx
+++ src/pages/PopularCategories/Dessert/Dessert.jsx
@@ -14,6 +14,7 @@ function Dessert() {
         bgImage={
           "https://readdy.ai/api/search-image?query=Decadent%20dessert%20display%20with%20chocolate%20cake%2C%20fruit%20tarts%2C%20macarons%2C%20and%20ice%20cream.%20Soft%2C%20dreamy%20lighting%20with%20bokeh%20effects.%20Desserts%20arranged%20on%20a%20marble%20surface%20with%20gold%20accents%20and%20fresh%20berries%20as%20garnish.%20Rich%20textures%20and%20colors%20highlighting%20the%20indulgent%2C%20tempting%20nature%20of%20sweet%20treats.&width=400&height=300&seq=10&orientation=landscape"
         }
+        title={`Enjoy your Dessert everyday with different flavors`}
       />
      <EachCategory filteredCategory={filteredCategory} exploreCategoryTitle={'Desserts'} />
     </>
